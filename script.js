fdb39cadec4e0422b1b67743bc919927f99048a5
olga.jasionek <ogi22021996@gmail.com>
2023-08-22 21:46:41 +0200

    fix, styles changes

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const currentYear = new Date().getFullYear();
-const galleryComponent = document.querySelector(
-  ".realizations-section_gallery"
-);
-const galleryBackdropComponent = document.querySelector(
+
+const bodyEl = document.body;
+const galleryEl = document.querySelector(".realizations-section_gallery");
+const galleryBackdropEl = document.querySelector(
   ".realizations-section_backdrop"
 );
-const mobileNavComponent = document.querySelector(".mobile-navigation_content");
-const bodyComponent = document.body;
-const searchInputDesctopComponent = document.querySelector(
-  ".navigation-searchbar_desctop"
+const mobileNavEl = document.querySelector(".mobile-navigation_content");
+const searchInputDesktopEl = document.querySelector(
+  ".navigation-searchbar_desktop"
 );
-const searchInputMobileComponent = document.querySelector(
+const searchInputMobileEl = document.querySelector(
   ".navigation-searchbar_mobile"
 );
 
 const showMoreImgBtn = document.getElementById("show-more");
-const menuMobileBtn = document.getElementById("moblie-menu-btn");
+const menuMobileBtn = document.getElementById("mobile-menu-btn");
 const closeMobileMenuBtn = document.getElementById("close-menu-btn");
-const showSearchInputDesctopBtn = document.getElementById(
-  "open-search-input-btn_desctop"
+const showSearchInputdesktopBtn = document.getElementById(
+  "open-search-input-btn_desktop"
 );
 const showSearchInputMobileBtn = document.getElementById(
   "open-search-input-btn_mobile"
 );
 
-const galleryItemsUrl = [
+const galleryItems = [
   { url: "public/img/gallery/Photo (1).png", alt: "Photo 1" },
   { url: "public/img/gallery/Photo (2).png", alt: "Photo 2" },
   { url: "public/img/gallery/Photo (3).png", alt: "Photo 3" },
@@ -34,32 +33,33 @@ const galleryItemsUrl = [
   { url: "public/img/gallery/Photo (7).png", alt: "Photo 7" },
   { url: "public/img/gallery/Photo (8).png", alt: "Photo 8" },
   { url: "public/img/gallery/Photo (9).png", alt: "Photo 9" },
-  { url: "public/img/gallery/Photo (1).png", alt: "Photo 1" },
-  { url: "public/img/gallery/Photo (2).png", alt: "Photo 2" },
-  { url: "public/img/gallery/Photo (3).png", alt: "Photo 3" },
-  { url: "public/img/gallery/Photo (4).png", alt: "Photo 4" },
-  { url: "public/img/gallery/Photo (5).png", alt: "Photo 5" },
-  { url: "public/img/gallery/Photo (6).png", alt: "Photo 6" },
-  { url: "public/img/gallery/Photo (7).png", alt: "Photo 7" },
-  { url: "public/img/gallery/Photo (8).png", alt: "Photo 8" },
-  { url: "public/img/gallery/Photo (9).png", alt: "Photo 9" },
+  { url: "public/img/gallery/Photo (1).png", alt: "Photo 11" },
+  { url: "public/img/gallery/Photo (2).png", alt: "Photo 12" },
+  { url: "public/img/gallery/Photo (3).png", alt: "Photo 13" },
+  { url: "public/img/gallery/Photo (4).png", alt: "Photo 14" },
+  { url: "public/img/gallery/Photo (5).png", alt: "Photo 15" },
+  { url: "public/img/gallery/Photo (6).png", alt: "Photo 16" },
+  { url: "public/img/gallery/Photo (7).png", alt: "Photo 17" },
+  { url: "public/img/gallery/Photo (8).png", alt: "Photo 18" },
+  { url: "public/img/gallery/Photo (9).png", alt: "Photo 19" },
 ];
 
-galleryItemsUrl.map(item => {
+galleryItems.map(item => {
   const itemWrapper = document.createElement("div");
-  itemWrapper.className = "gallery-item ";
   const itemImg = document.createElement("img");
+
+  itemWrapper.className = "gallery-item";
   itemImg.src = item.url;
   itemImg.alt = item.alt;
 
   itemWrapper.appendChild(itemImg);
-  galleryComponent.appendChild(itemWrapper);
+  galleryEl.appendChild(itemWrapper);
 });
 
 document.getElementById("currentYear").textContent = currentYear;
 
 window.onload = () => {
-  const galleryMasonry = new Masonry(galleryComponent, {
+  new Masonry(galleryEl, {
     querySelector: ".gallery-item",
     columnWidth: ".grid-sizer",
     gutter: ".gutter-sizer",
@@ -68,27 +68,25 @@ window.onload = () => {
 };
 
 showMoreImgBtn.addEventListener("click", () => {
-  galleryBackdropComponent.classList.remove("realizations-section_backdrop");
-  galleryBackdropComponent.classList.add(
-    "realizations-section_backdrop--hidden"
-  );
-  galleryComponent.classList.add("realizations-section_gallery--visible");
+  galleryBackdropEl.classList.remove("realizations-section_backdrop");
+  galleryBackdropEl.classList.add("realizations-section_backdrop--hidden");
+  galleryEl.classList.add("realizations-section_gallery--visible");
 });
 
 menuMobileBtn.addEventListener("click", () => {
-  mobileNavComponent.classList.add("mobile-navigation_content--active");
-  bodyComponent.classList.add("block-scroll");
+  mobileNavEl.classList.add("mobile-navigation_content--active");
+  bodyEl.classList.add("block-scroll");
 });
 
 closeMobileMenuBtn.addEventListener("click", () => {
-  mobileNavComponent.classList.remove("mobile-navigation_content--active");
-  bodyComponent.classList.remove("block-scroll");
+  mobileNavEl.classList.remove("mobile-navigation_content--active");
+  bodyEl.classList.remove("block-scroll");
 });
 
-showSearchInputDesctopBtn.addEventListener("click", () => {
-  searchInputDesctopComponent.classList.toggle("navigation-searchabr--show");
+showSearchInputdesktopBtn.addEventListener("click", () => {
+  searchInputDesktopEl.classList.toggle("navigation-searchbar--show");
 });
 
 showSearchInputMobileBtn.addEventListener("click", () => {
-  searchInputMobileComponent.classList.toggle("navigation-searchbar--show");
+  searchInputMobileEl.classList.toggle("navigation-searchbar--show");
 });
